7d6780e56caee212bd3c005b990c0d110b8d2eb8
Suyuimo <svewei8@icloud.com>
2024-06-09 21:57:02 +0200

    Inital Commit

---
+++ src/main/java/de/suyuimo/opencomputerscitybildaddon/OpenComputers_CityBild_Addon.java
@@ -0,0 +1,19 @@
+package de.suyuimo.opencomputerscitybildaddon;
+
+
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.CapabilityManager;
+import net.minecraftforge.common.capabilities.CapabilityToken;
+import net.minecraftforge.fml.common.Mod;
+
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(OpenComputers_CityBild_Addon.MODID)
+public class OpenComputers_CityBild_Addon {
+
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "opencomputers_citybild_addon";
+
+
+
+}
